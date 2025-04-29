83a9844cfb2523312d218e900ea40a06a03bce89
Abhishek Tiwari2 <abhishek.tiwari2@celebaltech.com>
2025-04-29 17:42:28 +0530

    fix: fixed the small tpe in book struct

--- models/book.go
+++ models/book.go
@@ -4,9 +4,9 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Book struct {
 	ID primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	Title string 	`bson:"title" json: "title"`
-	Author string	`bson:"author" json: "author"`
-	Year int        `bson:"year" json: "year"`
+	Title string `bson:"title" json:"title"`
+	Author string `bson:"author" json:"author"`
+	Year int `bson:"year" json:"year"`
 }
 
 
